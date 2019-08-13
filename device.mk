15460de2af6d65827b80694ba64c89f3b6d2b226
Christian Oder <myself5@carbonrom.org>
2019-08-13 23:15:00 +0200

    sm8150-common: Add Carbon configs
    sm8150-common: Update deps for lineage-17.1
    
    Change-Id: I4d8b69e44c38587f371ece8971517300e8f7f51b

--- device.mk
+++ device.mk
@@ -24,7 +24,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-carbon
 
 # Properties
 -include $(LOCAL_PATH)/system_prop.mk
