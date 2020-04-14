cb2ae9b97c66f207206c4ab9086a4f6038536809
Ashu7397 <ashutoshtaklikar@gmail.com>
2020-04-14 17:15:24 +0300

    raphael: Build Snap Camera

--- msmnile.mk
+++ msmnile.mk
@@ -134,6 +134,7 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     libdng_sdk.vendor \
+    Snap
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
